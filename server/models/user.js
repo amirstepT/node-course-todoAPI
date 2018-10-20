a3c30b32ba73bf71f5bdfc171350c0b03bb661c5
Amir Taghinia <amirstep@Amirs-iMac.home>
2018-10-19 21:42:27 -0400

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,54 @@
-var mongoose = require ('mongoose');
+const mongoose = require ('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
     trim: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-  }
+    unique: true,
+    validate: {
+      validator: validator.isEmail,  // same as (value) => { return validator.isEmail(value); },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access:  {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject(); // taking mongoose 'user' and converting to regular object where only properties avail in doc exist
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+  return user.save().then( () => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -28,17 +28,6 @@ var {User} = require('./models/user')
 var app = express();
 const port = process.env.PORT;
 
-var handleFn = (id, res, callback) => {
-  if (! ObjectID.isValid(id)) {
-    res.status(404).send('ID not valid');
-  } else {
-    callback(id).then((todo) => {
-      if (!todo) res.status(404).send('ID not found');
-      else res.send({todo});
-    }).catch((e) => res.status(400).send());
-  }
-};
-
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -107,6 +96,18 @@ app.patch('/todos/:id', (req, res) =>  {
       res.status(400).send();
     });
   }
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port, () => {
